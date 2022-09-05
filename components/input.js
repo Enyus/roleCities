77ecfd31cdd18e611a4b4fd3760035ac10be2e61
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2022-09-05 16:44:29 -0300

    alterando for para htmlFor em Input.js

--- components/input.js
+++ components/input.js
@@ -3,7 +3,7 @@ import styles from '../styles/Inputs.module.css'
 export default function Input(props) {
   return (
     <div className={styles.input__container}>
-        <label className={styles.input__label} for={props.id}>{props.label}</label>
+        <label className={styles.input__label} htmlFor={props.id}>{props.label}</label>
         <input type={props.type} id={props.id} className={styles.input__text} placeholder={props.placeholder}/>
     </div>
   )
