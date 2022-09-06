1fee3da177189443dcc8c1bebf7c16d26be5abcb
Enyus <enyus@hotmail.com>
2022-09-05 22:20:32 -0300

    teste 3

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ import Input from '../components/input'
 import Button from '../components/button'
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+// const SUPABASE_URL = process.env.SUPABASE_URL;
+// const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
 const supabaseClient = createClient(SUPABASE_URL,SUPABASE_ANON_KEY)
 
