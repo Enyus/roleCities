ef232014ffa69e2bd9f0caab6072e7fcd8f9eeb9
Cassio Sandrigo Santini <cassio.santini@educacao.sp.gov.br>
2022-12-27 10:06:48 -0300

    adicionando todas as imagens com display grid para melhor visualização

--- components/Grid.js
+++ components/Grid.js
@@ -11,35 +11,90 @@ function Grid(props) {
 
   switch (props.mostrar) {
     case "regiao":
-      imagemMostrada = <img src="/assentamento.png" alt="cidade" className={styles.imagem__cidade} onClick={handleClick} />;
+      imagemMostrada = (
+        <div
+          className={styles.imagem__regiao}
+          style={{ backgroundImage: `url(${props.background})` }}
+        >
+          <img
+            src="/assentamento.png"
+            alt="cidade"
+            className={styles.imagem__cidade}
+            onClick={handleClick}
+          />
+        </div>
+      );
       break;
     case "cidade":
-      imagemMostrada = <div className={styles.grid__construcoes}>
-        <img src="/construcoes/central-adm.png" className={styles.contrucao__item +" "+ styles.construcao__central} alt="Central Administrativa" />
-        <img src="/construcoes/banco.png" className={styles.contrucao__item +" "+ styles.construcao__banco} alt="Banco" />
-        <img src="/construcoes/estalagem.png" className={styles.contrucao__item +" "+ styles.construcao__estalagem} alt="Estalagem" />
-        <img src="/construcoes/mercado.png" className={styles.contrucao__item +" "+ styles.construcao__mercado} alt="Mercado" />
-        <img src="/construcoes/milicia.png" className={styles.contrucao__item +" "+ styles.construcao__milicia} alt="Milícia" />
-        <img src="/construcoes/mina.png" className={styles.contrucao__item +" "+ styles.construcao__mina} alt="Mina" />
-        <img src="/construcoes/porto.png" className={styles.contrucao__item +" "+ styles.construcao__porto} alt="Porto" />
-        <img src="/construcoes/templo.png" className={styles.contrucao__item +" "+ styles.construcao__templo} alt="Templo" />
-        <img src="/construcoes/torre-mago.png" className={styles.contrucao__item +" "+ styles.construcao__mago} alt="Torre do Mago" />
-        <img src="/construcoes/torre-vigia.png" className={styles.contrucao__item +" "+ styles.construcao__vigia} alt="Torre de Vigia" />
-      </div>;
+      imagemMostrada = (
+        <div
+          className={styles.grid__construcoes}
+          style={{ backgroundImage: `url(${props.background})` }}
+        >
+          <img
+            src="/construcoes/central-adm.png"
+            className={
+              styles.contrucao__item + " " + styles.construcao__central
+            }
+            alt="Central Administrativa"
+          />
+          <img
+            src="/construcoes/banco.png"
+            className={styles.contrucao__item + " " + styles.construcao__banco}
+            alt="Banco"
+          />
+          <img
+            src="/construcoes/estalagem.png"
+            className={
+              styles.contrucao__item + " " + styles.construcao__estalagem
+            }
+            alt="Estalagem"
+          />
+          <img
+            src="/construcoes/mercado.png"
+            className={
+              styles.contrucao__item + " " + styles.construcao__mercado
+            }
+            alt="Mercado"
+          />
+          <img
+            src="/construcoes/milicia.png"
+            className={
+              styles.contrucao__item + " " + styles.construcao__milicia
+            }
+            alt="Milícia"
+          />
+          <img
+            src="/construcoes/mina.png"
+            className={styles.contrucao__item + " " + styles.construcao__mina}
+            alt="Mina"
+          />
+          <img
+            src="/construcoes/porto.png"
+            className={styles.contrucao__item + " " + styles.construcao__porto}
+            alt="Porto"
+          />
+          <img
+            src="/construcoes/templo.png"
+            className={styles.contrucao__item + " " + styles.construcao__templo}
+            alt="Templo"
+          />
+          <img
+            src="/construcoes/torre-mago.png"
+            className={styles.contrucao__item + " " + styles.construcao__mago}
+            alt="Torre do Mago"
+          />
+          <img
+            src="/construcoes/torre-vigia.png"
+            className={styles.contrucao__item + " " + styles.construcao__vigia}
+            alt="Torre de Vigia"
+          />
+        </div>
+      );
       break;
   }
 
-  return (
-    <>
-      <div
-        className={styles.imagem__regiao}
-        style={{ backgroundImage: `url(${props.background})` }}
-      >
-        {imagemMostrada}
-
-      </div>
-    </>
-  );
+  return <>{imagemMostrada}</>;
 }
 
 export default Grid;
